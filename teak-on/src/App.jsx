73bab1f8c5d2eaf433ce251ce23a5107f9318b1f
meihui03 <mlok0006@student.monash.edu>
2025-07-21 19:43:37 +0800

    testing

--- teak-on/src/App.jsx
+++ teak-on/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+ return(
+  <main>
+    <div className = "pattern" />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className = "wrapper" >
+      <header>
+  
+        <h1> Find <span className= "text-gradient">Furniture</span> that you like</h1>
+      </header>
+
+      <p> Search </p>
+      <img src = "./search.png"/>
+    </div>
+  </main>
+ )
 }
 
 export default App
